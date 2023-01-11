21791af1b6808cbf3be49e5a09e9a3e6ca2f84ba
YashiroYsana <rochaelielvis@gmail.com>
2023-01-11 10:33:25 -0300

    Strings

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/* Some strings methods */
+
+// let someName = "  Teyla  ";
+
+// console.log(someName.charAt(2));
+// console.log(someName.indexOf("y"));
+// console.log(someName.lastIndexOf("l"));
+// console.log(someName.includes("e"));
+// console.log(someName.startsWith("T"));
+// console.log(someName.endsWith("a"));
+// console.log(someName.concat(" Shirley"));
+// console.log(someName.trim());
+// console.log(someName.repeat(2));
+
+// const templateName = `Gouim Talom`;
+// //const templateName = `Gouim Talom`;
+
+// console.log(`${templateName} is my big friend`);
